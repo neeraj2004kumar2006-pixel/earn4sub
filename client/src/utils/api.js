df3309aa2f0cb8987df54caef267708c8ad208a2
neeraj2004kumar2006-pixel <neeraj2004kumar2006@gmail.com>
2026-02-26 16:28:16 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 // utils/api.js - Axios API client for Sub4Earn
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE = (import.meta.env.VITE_API_URL || 'http://localhost:5000') + '/api';
 
 const api = axios.create({
   baseURL: API_BASE,
